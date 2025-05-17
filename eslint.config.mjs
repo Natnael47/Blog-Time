d66fd6223d4bb89a5e9aeabc34ab57320b784492
Natnael47 <nati47m10@gmail.com>
2025-05-18 00:17:56 +0300

    ignare in eslint,config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,21 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      ".next/**", // ⛔ Don't lint Next.js output
+      "lib/generated/prisma/**", // ⛔ Don't lint Prisma client
+      "node_modules/**", // ⛔ Don't lint dependencies
+      "**/*.js", // (Optional) Only lint .ts/.tsx files
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
